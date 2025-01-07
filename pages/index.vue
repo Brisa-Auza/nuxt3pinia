55b048ca1f0d539cc53ef1b88eed8e6ab44c1f22
Rey <reysoporte1@gmail.com>
2025-01-07 17:25:47 -0600

    prueba x3

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
   >
     <div style="position: relative; left: 20%; top: 20%">
       <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
-        <div class="text-subtitle-1 text-medium-emphasis">Usuario</div>
+        <div class="text-subtitle-1 text-medium-emphasis">Usuario ---O.o</div>
 
         <v-text-field
           density="compact"
