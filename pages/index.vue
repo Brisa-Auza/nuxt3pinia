2482efb04152a4a84506fda99bbd4eb22ed7a758
Brisa-Auza <you@example.com>
2025-01-07 17:01:07 -0600

    cambios en index

--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,64 @@
 <template>
-  <div>
-    <v-img
-      class="mx-auto my-6"
-      max-width="228"
-      src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
-    ></v-img>
+  <v-app
+    style="
+      background: url('/fondo5.PNG') no-repeat;
+      background-size: cover;
+      overflow: hidden;
+      width: 100%;
+      height: 100%;
+      position: relative;
+      display: flex;
+      flex-flow: row;
+    "
+  >
+    <div style="position: relative; left: 20%; top: 20%">
+      <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
+        <div class="text-subtitle-1 text-medium-emphasis">Usuario :D</div>
 
-    <v-card
-      class="mx-auto pa-12 pb-8"
-      elevation="8"
-      max-width="448"
-      rounded="lg"
-    >
-      <div class="text-subtitle-1 text-medium-emphasis">Usuario :D</div>
+        <v-text-field
+          density="compact"
+          placeholder="Usuario"
+          prepend-inner-icon="mdi-account"
+          variant="outlined"
+          v-model="usuario.username"
+        ></v-text-field>
 
-      <v-text-field
-        density="compact"
-        placeholder="Usuario"
-        prepend-inner-icon="mdi-account"
-        variant="outlined"
-        v-model="usuario.username"
-      ></v-text-field>
+        <div
+          class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+        >
+          Contraseña
+        </div>
 
-      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-        Contraseña
-
-      </div>
-
-      <v-text-field 
-        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="visible ? 'text' : 'password'"
-        density="compact"
-        placeholder="Contraseña"
-        prepend-inner-icon="mdi-lock-outline"
-        variant="outlined"
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Contraseña"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
           v-model="usuario.password"
-        @click:append-inner="visible = !visible"
-      ></v-text-field>
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
 
-    
-
-      <v-btn
-        class="mb-8"
-        color="blue"
-        size="large"
-        variant="tonal"
-        block
-        v-model="enviarL"
-      >
-        Ingresar
-      </v-btn>
-      <v-card
-        class="mb-12"
-        color="surface-variant"
-        variant="tonal"
-      >
-        <v-card-text class="text-medium-emphasis text-caption">
-          Copyright © 2025 STEELPRO V3.5.10 - Todos los derechos reservados Aviso de privacidad. Términos y condiciones
-        </v-card-text>
+        <v-btn
+          class="mb-8"
+          color="blue"
+          size="large"
+          variant="tonal"
+          block
+          v-model="enviarL"
+        >
+          Ingresar
+        </v-btn>
+        <v-card class="mb-12" color="surface-variant" variant="tonal">
+          <v-card-text class="text-medium-emphasis text-caption">
+            Copyright © 2025 STEELPRO V3.5.10 - Todos los derechos reservados Aviso de
+            privacidad. Términos y condiciones
+          </v-card-text>
+        </v-card>
       </v-card>
-    </v-card>
-  </div>
+    </div>
+  </v-app>
 </template>
 <script>
 import { useAuthStore } from "~/stores/auth";
