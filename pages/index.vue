b8bee06934551dd3a2e4c17e3ad8083690b3ff76
Brisa-Auza <you@example.com>
2025-01-07 18:09:56 -0600

    titulo

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
   >
     <div style="position: relative; left: 20%; top: 20%">
       <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
-        <div class="text-subtitle-1 text-medium-emphasis">Usuario de rey y brisa
+        <div class="text-subtitle-1 text-medium-emphasis">INICIAR SESION
         </div>
 
         <v-text-field
